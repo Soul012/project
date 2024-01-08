ba65ea780739c602a853fbcaaeabf29730bba47e
SOUL <67261074+Soul012@users.noreply.github.com>
2024-01-08 20:08:26 +0800

    Update ql_elm.sh

--- ql_elm.sh
+++ ql_elm.sh
@@ -1,5 +1,3 @@
-#cron: 30 8,12,16 * * *
-#const $ = new Env("饿了么");
 #!/bin/bash
 # new Env('萝卜-饿了么');
 # 环境变量 elmck  值：SID=xxxx; cookie2=xxxx;   （按格式来）
@@ -26,7 +24,7 @@ fi
 
 download_elm(){
 echo "开始下载elm二进制文件到$PWD/ELM目录"
-curl -sS -o $PWD/ELM/elm-$_ftype --create-dirs https://ghproxy.com/https://raw.githubusercontent.com/lu0b0/ELM/main/ELM/elm-$_ftype
+curl -sS -o $PWD/ELM/elm-$_ftype --create-dirs https://raw.githubusercontent.com/lu0b0/ELM/main/ELM/elm-$_ftype
 echo "下载完成，如需重新下载或更新请先删除该文件"
 if [ -f "$PWD/ELM/elm-$_ftype" ]; then
     echo "$PWD/ELM/elm-$_ftype"
